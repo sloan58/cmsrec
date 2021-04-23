7ee86db9bd3b25b37d0428355239ad8e363e9342
sloan58 <martin.sloan@karma-tek.com>
2021-04-23 16:04:09 -0400

    adjust recordings ui to use cards instead of table

--- resources/views/recordings/index.blade.php
+++ resources/views/recordings/index.blade.php
@@ -8,94 +8,37 @@
         @include('components.flash-message')
         <div class="row">
             <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header d-flex justify-content-between">
-                        <h4 class="card-title">My Recordings</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table">
-                                <thead class=" text-primary">
-                                <th>
-                                    Space Name
-                                </th>
-                                <th>
-                                    Filename
-                                </th>
-                                <th>
-                                    Size
-                                </th>
-                                <th>
-                                    Last Modified
-                                </th>
-                                <th class="text-right">
-                                    Actions
-                                </th>
-                                </thead>
-                                <tbody>
-                                @foreach($spacesWithRecordings as $space)
-                                    @foreach($space['recordings'] as $recording)
-                                        <tr>
-                                            <td>
-                                                {{ $space['spaceName'] }}
-                                            </td>
-                                            <td>
-                                                {{ $recording['baseName'] }}
-                                            </td>
-                                            <td>
-                                                {{ $recording['fileSize'] }}
-                                            </td>
-                                            <td>
-                                                {{ $recording['lastModified'] }}
-                                            </td>
-                                            <td class="text-right">
-                                                <button class="btn btn-success btn-fab btn-icon btn-round watchVideo" data-video="{{ $recording['sanitizedFilename'] }}">
-                                                    <i class="fa fa-video-camera"></i>
-                                                </button>
-                                                <a href="/recordings/download?file={{ $recording['url'] }}" class="btn btn-info btn-fab btn-icon btn-round">
-                                                    <i class="fa fa-download"></i>
-                                                </a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td></td>
-                                            <td></td>
-                                            <td>
-                                                <video id="{{ $recording['sanitizedFilename'] }}" controls width="500" style="display: none;">
-                                                    <source src="/recordings/play?file={{ $recording['url'] }}"
-                                                            type="video/mp4"
-                                                    >
-                                                    Sorry, your browser doesn't support embedded videos.
-                                                </video>
-                                            </td>
-                                            <td></td>
-                                            <td></td>
-                                        </tr>
-                                    @endforeach
-                                @endforeach
-                                </tbody>
-                            </table>
+                @foreach($spacesWithRecordings as $space)
+                <h1 class="display-4">{{ $space['spaceName'] }}</h1>
+                <hr>
+                <div class='col-sm-12 col-md-10 col-lg-12 justify-content-between'>
+                    <div class='row row-cols-1 row-cols-md-4'>
+                        @foreach($space['recordings'] as $recording)
+                        <div class="col mb-4" style="max-width: 313px;">
+                            <div class="card h-100 shadow">
+                                <video id="{{ $recording['sanitizedFilename'] }}" preload="none" controls>
+                                    <source src="/recordings/play?file={{ $recording['url'] }}"
+                                            type="video/mp4"
+                                    >
+                                    Sorry, your browser doesn't support embedded videos.
+                                </video>
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $recording['baseName'] }}</h5>
+                                    <p class="card-text">in <b>{{ $space['spaceName'] }}</b></p>
+                                    <footer class="blockquote-footer"><b>Created:</b> {{ $recording['lastModified'] }}</footer>
+                                    <footer class="blockquote-footer"><b>Size:</b> {{ $recording['fileSize'] }}</footer>
+                                </div>
+                                <div class="card-footer text-left">
+                                    <a href="/recordings/download?file={{ $recording['url'] }}" class="btn btn-primary"><i class="fa fa-download mr-2"></i>Download</a>
+                                </div>
+                            </div>
                         </div>
+                        @endforeach
                     </div>
                 </div>
+                @endforeach
             </div>
         </div>
     </div>
-
-    @push('scripts')
-        <script>
-            $(document).on('click','.watchVideo',function(){
-                const video = $(this).attr('data-video');
-                const videoElement = $(`#${video}`)
-                if(videoElement.is(":hidden")) {
-                    videoElement.show();
-                    videoElement.trigger('play');
-                } else {
-                    videoElement.hide();
-                    videoElement.trigger('pause');
-                }
-            });
-        </script>
-    @endpush
 
 @endsection
