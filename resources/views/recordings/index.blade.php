a9d3aadb05fa883d48ec512ffd36b356c02deaf8
sloan58 <martin.sloan@karma-tek.com>
2021-04-27 12:29:59 -0400

    center recordings in ui

--- resources/views/recordings/index.blade.php
+++ resources/views/recordings/index.blade.php
@@ -7,12 +7,12 @@
     <div class="content">
         @include('components.flash-message')
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-12 text-center">
                 @foreach($cmsCoSpaces as $space)
                     @if($space->cmsRecordings()->count())
                         <h1 class="display-4 mb-3"><span style="border-bottom: 2px solid #ddd;">{{ $space->name }}</span></h1>
                         <div class='col-sm-12 col-md-10 col-lg-12 justify-content-between'>
-                            <div class='row row-cols-1 row-cols-md-4'>
+                            <div class='row row-cols-1 row-cols-md-4 justify-content-center'>
                                 @foreach($space->cmsRecordings as $recording)
                                     <livewire:cms-recording-card :recording="$recording"/>
                                 @endforeach
