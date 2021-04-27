8cb511b1c80cf86a5113cd73f657e46d0241df21
sloan58 <martin.sloan@karma-tek.com>
2021-04-27 17:41:01 -0400

    enhance recording search

--- app/Http/Livewire/CmsRecordingsPage.php
+++ app/Http/Livewire/CmsRecordingsPage.php
@@ -8,11 +8,27 @@ use App\Models\CmsRecording;
 class CmsRecordingsPage extends Component
 {
     public $term = '';
+    public $searchBy = 'Recording Name';
 
     public function render()
     {
-        $cmsRecordings = CmsRecording::when($this->term, function ($query) {
-            $query->where('filename', 'like', '%' . $this->term . '%');
+
+        $cmsRecordings = CmsRecording::with(['cmsCoSpace', 'owner'])->when($this->term, function ($query) {
+            switch($this->searchBy) {
+                case 'Recording Name':
+                    $query->where('filename', 'like', '%' . $this->term . '%');
+                    break;
+                case 'Space Name':
+                    $query->whereHas('cmsCoSpace', function($query) {
+                        $query->where('name', 'like', '%' . $this->term . '%');
+                    });
+                    break;
+                case 'Owner Name':
+                    $query->whereHas('owner', function($query) {
+                        $query->where('name', 'like', '%' . $this->term . '%');
+                    });
+                    break;
+            }
         });
 
         if(!auth()->user()->isAdmin()) {
