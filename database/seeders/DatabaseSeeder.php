970fec3377a4bd2c8d48087348e3625896f0a346
sloan58 <martin.sloan@karma-tek.com>
2021-05-24 15:22:53 -0400

    adjust seeders for prod

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             CmsSeeder::class,
-            CmsCoSpaceSeeder::class,
-            CmsRecordingSeeder::class
+//            CmsCoSpaceSeeder::class,
+//            CmsRecordingSeeder::class
         ]);
     }
 }
--- database/settings/2021_05_13_192211_nfs_storage_settings.php
+++ database/settings/2021_05_13_192211_nfs_storage_settings.php
@@ -6,15 +6,18 @@ class NfsStorageSettings extends SettingsMigration
 {
     public function up(): void
     {
-        try {
-            $jsonSeeds = json_decode(file_get_contents(base_path('/privateSeeder.json')));
-            $this->migrator->add('nfs.host', $jsonSeeds->nfs->host);
-            $this->migrator->add('nfs.path', $jsonSeeds->nfs->path);
-            $this->migrator->add('nfs.mnt_view', $jsonSeeds->nfs->mnt_view);
-        } catch(Exception $e) {
-            $this->migrator->add('nfs.host', '');
-            $this->migrator->add('nfs.path', '');
-            $this->migrator->add('nfs.mnt_view', []);
-        }
+//        try {
+//            $jsonSeeds = json_decode(file_get_contents(base_path('/privateSeeder.json')));
+//            $this->migrator->add('nfs.host', $jsonSeeds->nfs->host);
+//            $this->migrator->add('nfs.path', $jsonSeeds->nfs->path);
+//            $this->migrator->add('nfs.mnt_view', $jsonSeeds->nfs->mnt_view);
+//        } catch(Exception $e) {
+//            $this->migrator->add('nfs.host', '');
+//            $this->migrator->add('nfs.path', '');
+//            $this->migrator->add('nfs.mnt_view', []);
+//        }
+        $this->migrator->add('nfs.host', '');
+        $this->migrator->add('nfs.path', '');
+        $this->migrator->add('nfs.mnt_view', []);
     }
 }
