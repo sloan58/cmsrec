d6a4fecd565b8162646d6859a24dda49c12ca9df
sloan58 <martin.sloan@karma-tek.com>
2021-06-17 17:16:08 -0400

    add percent complete logging for CMS syncs

--- app/APIs/CmsRest.php
+++ app/APIs/CmsRest.php
@@ -61,11 +61,18 @@ class CmsRest
      */
     private function queryCmsApi($uri)
     {
+        logger()->debug("CmsRest@queryCmsApi ({$this->cms->host}): Initiating query", [
+            'uri' => $uri
+        ]);
         try {
             $response = $this->client->get($uri);
             $xml = simplexml_load_string($response->getBody()->getContents());
             $json = json_encode($xml);
 
+            logger()->debug("CmsRest@queryCmsApi ({$this->cms->host}): Received successful response");
+            logger()->debug("CmsRest@queryCmsApi ({$this->cms->host}): JSON response", [
+                'response' => $json
+            ]);
             return json_decode($json, true);
 
         } catch (RequestException $e) {
@@ -85,9 +92,16 @@ class CmsRest
      */
     public function getCoSpaces()
     {
+        info("CmsRest@getCoSpaces ({$this->cms->host}): Collecting CMS CoSpaces");
+
         $offset = 0;
         $limit = 1;
         $now = now();
+
+        info("CmsRest@getCoSpaces ({$this->cms->host}): Set initial query params", [
+            'offset' => $offset,
+            'limit' => $limit,
+        ]);
 
         $response = $this->queryCmsApi("/api/v1/coSpaces?offset={$offset}&limit={$limit}");
 
@@ -95,10 +109,22 @@ class CmsRest
         $limit = 20;
         $iterations = (int) ceil((int) $total / $limit);
 
+        info("CmsRest@getCoSpaces ({$this->cms->host}): Calculated query iterations", [
+            'total' => $total,
+            'limit' => $limit,
+            'iterations' => $iterations
+        ]);
+
+        info("CmsRest@getCoSpaces ({$this->cms->host}): Iterating API");
         for($i = 1; $i <= $iterations; $i++) {
             $response = $this->queryCmsApi("/api/v1/coSpaces?offset={$offset}&limit={$limit}");
             foreach($response['coSpace'] as $coSpace) {
+                logger()->debug("CmsRest@getCoSpaces ({$this->cms->host}): Collecting CoSpace details", [
+                    'coSpace' => $coSpace
+                ]);
                 $response = $this->queryCmsApi("/api/v1/coSpaces/{$coSpace['@attributes']['id']}");
+
+                logger()->debug("CmsRest@getCoSpaces ({$this->cms->host}): Updating model");
                 CmsCoSpace::updateOrCreate([
                     'space_id' =>  $response['@attributes']['id']],
                     [
@@ -108,9 +134,17 @@ class CmsRest
                 )->touch();
             }
             $offset += $limit;
+            logger()->debug("CmsRest@getCoSpaces ({$this->cms->host}): Iterating offset", [
+                'offset' => $offset
+            ]);
 
+            $percentFinished = (int) floor((($i / $iterations) * 100));
+            if($percentFinished % 10 === 0) {
+                info("CmsRest@getCoSpaces ({$this->cms->host}): $percentFinished% done");
+            }
         }
 
+        info("CmsRest@queryCmsApi ({$this->cms->host}): Removing stale accounts");
         CmsCoSpace::where('updated_at', '<', $now)->delete();
     }
 
@@ -119,24 +153,39 @@ class CmsRest
      */
     public function getCmsUserIds()
     {
+        info("CmsRest@getCmsUserIds ({$this->cms->host}): Collecting CMS User ID's");
+
         $offset = 0;
         $limit = 1;
         $now = now();
 
+        info("CmsRest@getCmsUserIds ({$this->cms->host}): Set initial query params", [
+            'offset' => $offset,
+            'limit' => $limit,
+        ]);
+
         $response = $this->queryCmsApi("/api/v1/users?offset={$offset}&limit={$limit}");
 
         $total = $response['@attributes']['total'];
-
         $limit = 20;
-
         $iterations = (int) ceil((int) $total / $limit);
 
+        info("CmsRest@getCmsUserIds ({$this->cms->host}): Calculated query iterations", [
+            'total' => $total,
+            'limit' => $limit,
+            'iterations' => $iterations
+        ]);
+
+        info("CmsRest@getCmsUserIds ({$this->cms->host}): Iterating API");
         for($i = 1; $i <= $iterations; $i++) {
             $response = $this->queryCmsApi("/api/v1/users?offset={$offset}&limit={$limit}");
-
             foreach($response['user'] as $user) {
+                logger()->debug("CmsRest@getCmsUserIds ({$this->cms->host}): Collecting User details", [
+                    'coSpace' => $user
+                ]);
                 $response = $this->queryCmsApi("/api/v1/users/{$user['@attributes']['id']}");
                 if($response['email']) {
+                    logger()->debug("CmsRest@getCmsUserIds ({$this->cms->host}): Updating model");
                     User::updateOrCreate(
                         ['email' => $response['email']],
                         [
@@ -146,9 +195,19 @@ class CmsRest
                     )->touch();
                 }
             }
+
             $offset += $limit;
+            logger()->debug("CmsRest@getCmsUserIds ({$this->cms->host}): Iterating offset", [
+                'offset' => $offset
+            ]);
+
+            $percentFinished = (int) floor((($i / $iterations) * 100));
+            if($percentFinished % 10 === 0) {
+                info("CmsRest@getCmsUserIds ({$this->cms->host}): $percentFinished% done");
+            }
         }
 
+        info("CmsRest@getCmsUserIds ({$this->cms->host}): Removing stale accounts");
         User::where('cms_owner_id', '!=', '')->where('updated_at', '<', $now)->delete();
     }
 }
