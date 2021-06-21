82efd64c9aa924b184dcf857f8b033a74340d767
sloan58 <martin.sloan@karma-tek.com>
2021-06-21 11:34:53 -0400

    add logging to new nfs monitor command

--- app/Console/Commands/MonitorNfsMount.php
+++ app/Console/Commands/MonitorNfsMount.php
@@ -35,8 +35,10 @@ class MonitorNfsMount extends Command
      */
     public function handle()
     {
+        info("MonitorNfsMount@handle: Running NFS Check");
         $nfsSettings = app(\App\Settings\NfsSettings::class);
         if (strlen($nfsSettings->host) && !$nfsSettings->mountExists()) {
+            logger()->error("MonitorNfsMount@handle: NFS mount not found!  Sending email to app admins");
             \Mail::raw("NFS Mount not found for host {$nfsSettings->host} at {$nfsSettings->path}", function($message) {
                 $message->setPriority(\Swift_Message::PRIORITY_HIGH);
                 $message->subject('NFS Mount Not Found');
